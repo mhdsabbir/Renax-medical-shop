20da0e8c32094ab00e060159b4476dffb35d5052
Mhd sabbir <muhammadsabbir666@gmail.com>
2021-10-17 22:45:05 +0600

    add1d

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBQaZKP91NohvGznfHL52mb117SuJCN7tg",
+    authDomain: "genius-341df.firebaseapp.com",
+    projectId: "genius-341df",
+    storageBucket: "genius-341df.appspot.com",
+    messagingSenderId: "868752407375",
+    appId: "1:868752407375:web:9b9468a244dce47c0aa33a"
+  };
+
+export default firebaseConfig;
